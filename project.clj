44d15cd3eeeb24ed51b6035f086246301755c990
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2012-08-15 08:24:04 +0700

    Change :license, last commit was wrong

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.namespace "0.1.3"]]
   :url "https://github.com/bpdp/ring-reload-modified"
-  :license "MIT License")
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"})
