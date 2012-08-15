2f50625d577fa5d1ce618f73152a75aef983bc50
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2012-08-15 08:16:04 +0700

    Add :url and :license to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject ring-reload-modified "0.1.2"
   :description "Middleware to reload modified namespaces."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.namespace "0.1.3"]])
+                 [org.clojure/tools.namespace "0.1.3"]]
+  :url "https://github.com/bpdp/ring-reload-modified"
+  :license "MIT License")
