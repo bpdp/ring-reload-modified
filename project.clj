267c5ad4863d5e8b7a9a435f59fc3a0943d238a2
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2012-08-15 08:44:39 +0700

    Change project.clj, put org.clojars.bpdp so that I can put it into clojars repo

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-reload-modified "0.1.2"
+(defproject org.clojars.bpdp/ring-reload-modified "0.1.2"
   :description "Middleware to reload modified namespaces."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.namespace "0.1.3"]]
